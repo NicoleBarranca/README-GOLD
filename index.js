d56ce8351c2288344681fb8175122c8ece594f84
Nicole Barranca <nikkibarranca@gmail.com>
2022-02-28 18:57:39 -0500

    generate markdown

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const fs = require('fs');
+// const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 
+
+
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [ 
+//     {
+//     type: 'input',
+//     name: 'name',
+//     message: 'What is the name of your project? (Required)'},
+//     {
+//         type: 'confirm',
+//         name: 'badges',
+//         message: 'Would you like to add a badge?',
+//         default: false
+//         // if yes info for badges
+//     },
+//     {
+//         type: 'confirm',
+//         name: 'badges',
+//         message: 'Would you like to add a badge?',
+//         default: false
+//     }
+// ];
+
+
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -18,28 +41,27 @@ init();
 
 //example of promises
 
-const inquirer = require("inquirer");
 
-function promptUser() {
-    return inquirer.prompt(questions[{
-        name: "fistname",
-        type: "input",
-        message: "Enter first name",
-        validate(input,) {
-            if(input.matches(/\.js/)){
-                return "invalid anem";
-            }
-            return true;
-        }
-    }
-])
-}
+// function promptUser() {
+//     return inquirer.prompt(questions[{
+//         name: "fistname",
+//         type: "input",
+//         message: "Enter first name",
+//         validate(input,) {
+//             if(input.matches(/\.js/)){
+//                 return "invalid anem";
+//             }
+//             return true;
+//         }
+//     }
+// ])
+// }
 
-promptUser()
-.then((response) => {
-    console.log(response);
-})
-.catch((err) =>{
-    console.log(err.message);
-    process.exit();
-});
+// promptUser()
+// .then((response) => {
+//     console.log(response);
+// })
+// .catch((err) =>{
+//     console.log(err.message);
+//     process.exit();
+// });
