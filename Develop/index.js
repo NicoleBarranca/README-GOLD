a1e965d0940512460f0f9c0c624f852f28e5f914
Nicole Barranca <nikkibarranca@gmail.com>
2022-02-28 09:13:55 -0500

    packages

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,6 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -11,3 +13,32 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+
+//example of promises
+
+const inquirer = require("inquirer");
+
+function promptUser() {
+    return inquirer.prompt(questions[{
+        name: "fistname",
+        type: "input",
+        message: "Enter first name",
+        validate(input,) {
+            if(input.matches(/\.js/)){
+                return "invalid anem";
+            }
+            return true;
+        }
+    }
+])
+}
+
+promptUser()
+.then((response) => {
+    console.log(response);
+})
+.catch((err) =>{
+    console.log(err.message);
+    process.exit();
+});
