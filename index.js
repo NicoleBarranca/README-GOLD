a9bc6c6364eaeb6f5dc8f1ada97ca2a7a507a35c
Nicole Barranca <nikkibarranca@gmail.com>
2022-02-28 19:07:12 -0500

    init

--- index.js
+++ index.js
@@ -1,46 +1,56 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
 const inquirer = require('inquirer');
-// const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 
 
 // TODO: Create an array of questions for user input
-// const questions = [ 
-//     {
-//     type: 'input',
-//     name: 'name',
-//     message: 'What is the name of your project? (Required)'},
-//     {
-//         type: 'confirm',
-//         name: 'badges',
-//         message: 'Would you like to add a badge?',
-//         default: false
-//         // if yes info for badges
-//     },
-//     {
-//         type: 'confirm',
-//         name: 'badges',
-//         message: 'Would you like to add a badge?',
-//         default: false
-//     }
-// ];
+const questions = [ 
+    {
+    type: 'input',
+    name: 'name',
+    message: 'What is the name of your project? (Required)'},
+    {
+        type: 'confirm',
+        name: 'badges',
+        message: 'Would you like to add a badge?',
+        default: false
+        // if yes info for badges
+    },
+    {
+        type: 'confirm',
+        name: 'badges',
+        message: 'Would you like to add a badge?',
+        default: false
+    }
+];
 
 
 
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// // TODO: Create a function to initialize app
+// function init() {}
 
-// Function call to initialize app
+// // Function call to initialize app
+// init();
+
+function init() {
+    inquirer.prompt(questions)
+    .then((inquirerResponse, data) => {
+        console.log("Making README");
+        fs.writeFileSync("README.MD, inquirerResponse, data");
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+}
+
 init();
-
-
 //example of promises
-
 
 // function promptUser() {
 //     return inquirer.prompt(questions[{
